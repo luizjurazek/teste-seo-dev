5f14fa0549fcfb8940aee0d5ffe268b83dd51678
Luiz Jurazek <luizjurazek@gmail.com>
2024-06-29 11:48:28 -0300

    feat: adjust the script to return to pattern layout in case studies

--- script.js
+++ script.js
@@ -5,19 +5,26 @@ btns_case.forEach(button => {
   
   button.addEventListener('click', e => {
     e.preventDefault()
-    btns_case.forEach(button => button.classList.remove('case-studies-active-menu'))
-    e.currentTarget.classList.add('case-studies-active-menu');
+    // Caso o botao já esteja ativo remove todas as classes que fazem modificacao e volta ao padrao
+    if(button.classList.contains('case-studies-active-menu')){
+      console.log("tem")
+      e.currentTarget.classList.remove('case-studies-active-menu')
+      itens_case_studies.forEach(item => item.classList.remove('desactive-item-studies'))
+      itens_case_studies.forEach(item => item.classList.remove('active-item-studies'))
+    } else {
+      // Caso contrario ativa a classe do menu e exibe somente o elemento clicado
+      btns_case.forEach(button => button.classList.remove('case-studies-active-menu'))
+      e.currentTarget.classList.add('case-studies-active-menu');
 
-    const dataItem = e.currentTarget.getAttribute('data-item');
+      const dataItem = e.currentTarget.getAttribute('data-item');
   
-    const item_selected = document.getElementById(dataItem)
-    console.log(item_selected)
+      const item_selected = document.getElementById(dataItem)
 
-    itens_case_studies.forEach(item => {
-      item.classList.add("desactive-item-studies")
-      
-    })
-    item_selected.classList.remove("desactive-item-studies")
-    item_selected.classList.add("active-item-studies")
+      itens_case_studies.forEach(item => {
+        item.classList.add("desactive-item-studies") 
+      })
+      item_selected.classList.remove("desactive-item-studies")
+      item_selected.classList.add("active-item-studies")
+    }    
   })
 })
